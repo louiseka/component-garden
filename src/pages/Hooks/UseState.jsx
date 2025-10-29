b803e433edf4dc693dbed313b5e92ce049176cc0
Louise Aldridge <louisealdridge88@gmail.com>
2025-10-29 20:58:34 +0000

    Styling plant growing and layout

--- src/pages/Hooks/UseState.jsx
+++ src/pages/Hooks/UseState.jsx
@@ -1,10 +1,14 @@
 import PlantGrowth from "../../components/Hooks/useState/PlantGrowth";
+import styles from "./UseState.module.css";
 
 export default function UseState() {
   return (
-    <div>
-      <p>This is useState page</p>
-      <h3>Water the Plant</h3>
+    <div className={styles.wrapper}>
+      <div className={styles.text}>
+        <h3>Water the Plant</h3>
+
+        <p>This is useState page</p>
+      </div>
 
       <PlantGrowth />
     </div>
