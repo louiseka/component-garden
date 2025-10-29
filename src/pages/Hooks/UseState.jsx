da585e6931ea339954fda0146ddfbfa07bb6f927
Louise Aldridge <louisealdridge88@gmail.com>
2025-10-29 12:49:58 +0000

    Rendering component

--- src/pages/Hooks/UseState.jsx
+++ src/pages/Hooks/UseState.jsx
@@ -1,7 +1,10 @@
+import PlantGrowth from "../../components/Hooks/useState/PlantGrowth";
+
 export default function UseState() {
   return (
     <div>
       <p>This is useState page</p>
+      <PlantGrowth />
     </div>
   );
 }
