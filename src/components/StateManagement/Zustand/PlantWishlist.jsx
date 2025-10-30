cb2d11fb13bfa9299a223ac89c892bb805517105
Louise Aldridge <louisealdridge88@gmail.com>
2025-10-30 15:39:11 +0000

    Semantic form additions and styling

--- src/components/StateManagement/Zustand/PlantWishlist.jsx
+++ src/components/StateManagement/Zustand/PlantWishlist.jsx
@@ -1,6 +1,8 @@
 import { create } from "zustand";
 import { useState } from "react";
 
+import styles from "./PlantWishlist.module.css";
+
 const usePlantStore = create((set) => ({
   plants: ["Monstera"],
   addPlant: (plant) => set((state) => ({ plants: [...state.plants, plant] })),
@@ -18,26 +20,37 @@ export default function PlantWishlist() {
   };
 
   return (
-    <section>
-      <form onSubmit={handleSubmit}>
-        <label htmlFor="wishlist">Add a plant to your wishlist:</label>
-        <input
-          type="text"
-          placeholder="Daisy"
-          value={newPlant}
-          onChange={(e) => setNewPlant(e.target.value)}
-          name="wishlist"
-          id="wishlist"
-          required
-        />
-        <button type="submit">Add Plant</button>
-      </form>
-
-      <ul>
-        {plants.map((plant) => (
-          <li key={plant}>{plant}</li>
-        ))}
-      </ul>
+    <section className={styles.wrapper}>
+      <div>
+        <form onSubmit={handleSubmit}>
+          <fieldset className={styles.form_fieldset}>
+            <label className={styles.form_label} htmlFor="wishlist">
+              Add a plant to your wishlist:
+            </label>
+            <input
+              className={styles.form_input}
+              type="text"
+              placeholder="Daisy"
+              value={newPlant}
+              onChange={(e) => setNewPlant(e.target.value)}
+              name="wishlist"
+              id="wishlist"
+              required
+            />
+          </fieldset>
+          <button className={styles.form_button} type="submit">
+            Add Plant
+          </button>
+        </form>
+      </div>
+      <div className={styles.list}>
+        <h3>Your plant wishlist</h3>
+        <ul>
+          {plants.map((plant) => (
+            <li key={plant}>{plant}</li>
+          ))}
+        </ul>
+      </div>
     </section>
   );
 }
