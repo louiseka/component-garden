3428581bef478c7be3c5427e33681d5b342c1d18
Louise Aldridge <louisealdridge88@gmail.com>
2025-10-30 15:39:38 +0000

    Adding short description

--- src/pages/StateManagement/Zustand.jsx
+++ src/pages/StateManagement/Zustand.jsx
@@ -1,9 +1,17 @@
 import PlantWishlist from "../../components/StateManagement/Zustand/PlantWishlist";
+import styles from "./Zustand.module.css";
 
 export default function Zustand() {
   return (
     <section>
-      <p>This is Zustand page</p>
+      <div className={styles.text}>
+        <h3>Remember to water your plant!</h3>
+        <p>
+          A form-based component that lets users add plants to a shared wishlist
+          using Zustand for state management. New plants are added instantly and
+          displayed in a styled list.
+        </p>
+      </div>
       <PlantWishlist />
     </section>
   );
