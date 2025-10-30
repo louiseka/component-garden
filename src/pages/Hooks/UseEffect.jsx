352fe988e543b4e0958f8221803810562aad771f
Louise Aldridge <louisealdridge88@gmail.com>
2025-10-30 11:16:46 +0000

    Adding short description

--- src/pages/Hooks/UseEffect.jsx
+++ src/pages/Hooks/UseEffect.jsx
@@ -1,9 +1,18 @@
 import WateringReminder from "../../components/Hooks/useEffect/WateringReminder";
 
+import styles from "./UseEffect.module.css";
+
 export default function UseEffect() {
   return (
-    <div>
-      <p>This is useEffect page</p>
+    <div className={styles.wrapper}>
+      <div className={styles.text}>
+        <h3>Remember to water your plant!</h3>
+        <p>
+          Uses useEffect with setTimeout to show a reminder after 5 seconds of
+          inactivity and setInterval to run a live timer, demonstrating timed
+          side effects and dynamic UI updates in React.
+        </p>
+      </div>
       <WateringReminder />
     </div>
   );
