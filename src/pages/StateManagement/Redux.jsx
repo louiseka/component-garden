ef8d5a683dd009a71781eebe65baa4b9a49b8aed
Louise Aldridge <louisealdridge88@gmail.com>
2025-11-04 11:36:15 +0000

    rendering water reminder component

--- src/pages/StateManagement/Redux.jsx
+++ src/pages/StateManagement/Redux.jsx
@@ -1,7 +1,15 @@
+import { Provider } from "react-redux";
+import { store } from "../../state/store";
+
+import WateringReminder from "../../components/StateManagement/Redux/WateringReminder";
+
 export default function Redux() {
   return (
     <section>
       <p>This is Redux page</p>
+      <Provider store={store}>
+        <WateringReminder />
+      </Provider>
     </section>
   );
 }
