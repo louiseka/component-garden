e0543a76de9a553ca1339068968a958d7611c5bf
Louise Aldridge <louisealdridge88@gmail.com>
2025-11-04 13:37:05 +0000

    Creating a form and input

--- src/components/StateManagement/Redux/WateringReminder.jsx
+++ src/components/StateManagement/Redux/WateringReminder.jsx
@@ -1,19 +1,50 @@
 import { useDispatch, useSelector } from "react-redux";
 import { addPlant } from "../../../state/store";
 
+import styles from "./WateringReminder.module.css";
+
 export default function WateringReminder() {
   const plants = useSelector((state) => state.plants);
   const dispatch = useDispatch();
 
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    const plantName = e.target.elements.reminder.value;
+    dispatch(addPlant(plantName.trim()));
+    e.target.reset();
+  };
+
   return (
-    <div>
-      <p>Watering reminder component</p>
-      <button onClick={() => dispatch(addPlant("Rose"))}>Add Rose</button>
-      <ul>
-        {plants.map((plant) => (
-          <li key={plant}>{plant}</li>
-        ))}
-      </ul>
-    </div>
+    <section className={styles.wrapper}>
+      <div>
+        <form onSubmit={handleSubmit}>
+          <fieldset className={styles.form_fieldset}>
+            <label className={styles.form_label} htmlFor="reminder">
+              Add a plant to your Watering Reminder list
+            </label>
+            <input
+              className={styles.form_input}
+              type="text"
+              name="reminder"
+              id="reminder"
+              placeholder="Rose"
+              required
+            />
+          </fieldset>
+          <button type="submit" className={styles.form_button}>
+            Add a reminder
+          </button>
+        </form>
+      </div>
+      <div className={styles.list}>
+        <h3>Remember to water</h3>
+
+        <ul>
+          {plants.map((plant) => (
+            <li key={plant}>{plant}</li>
+          ))}
+        </ul>
+      </div>
+    </section>
   );
 }
