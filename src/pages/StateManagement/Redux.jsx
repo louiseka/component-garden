e102cff7f29240d3cdb2d3e79f708ae8db41eacf
Louise Aldridge <louisealdridge88@gmail.com>
2025-11-04 13:36:37 +0000

    describing redux use

--- src/pages/StateManagement/Redux.jsx
+++ src/pages/StateManagement/Redux.jsx
@@ -2,11 +2,19 @@ import { Provider } from "react-redux";
 import { store } from "../../state/store";
 
 import WateringReminder from "../../components/StateManagement/Redux/WateringReminder";
+import styles from "./Redux.module.css";
 
 export default function Redux() {
   return (
     <section>
-      <p>This is Redux page</p>
+      <div className={styles.text}>
+        <h3>Your watering reminder</h3>
+        <p>
+          Watering Reminder lets users add plant reminders. It uses Redux
+          Toolkit to manage the global list of plants, with useSelector to read
+          state and useDispatch to add new plants efficiently.
+        </p>
+      </div>
       <Provider store={store}>
         <WateringReminder />
       </Provider>
