9a59506ad5d8e2744c0c4026011e3178c2226033
Louise Aldridge <louisealdridge88@gmail.com>
2025-12-18 15:30:23 +0000

    Creating card and updating layout

--- src/components/Components/Card/Card.jsx
+++ src/components/Components/Card/Card.jsx
@@ -3,7 +3,12 @@ import styles from "./Card.module.css";
 export default function Card() {
   return (
     <div className={styles.cardContainer}>
-      <p>This is a card</p>
+      <img className={styles.cardImage} src="./images/flower.svg" />
+
+      <h4 className={styles.cardTitle}>Card Title</h4>
+      <a className={styles.cardLink} href="#">
+        View Component
+      </a>
     </div>
   );
 }
--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -2,34 +2,29 @@ import styles from "./Home.module.css";
 import Card from "../../components/Components/Card/Card";
 
 export default function Home() {
+  const cardData = [
+    { title: "Water Flower", imgUrl: "", urlSlug: "" },
+    { title: "Look after Flower", imgUrl: "", urlSlug: "" },
+    { title: "Garden Gate", imgUrl: "", urlSlug: "" },
+  ];
+
   return (
     <section className={styles.wrapper}>
-      <div className={styles.hero}>
-        <div>
-          <h1 className={styles.header}>
-            A garden where I grow React components.
-          </h1>
-          <p>
-            Learning, experimenting, and cultivating better code—one component
-            at a time.
-          </p>
-          <button className={styles.ctaButton}>Explore the Garden</button>
-        </div>
-        <div className={styles.imageContainer}>
-          <div className={styles.blob}></div>
-          <img
-            className={styles.seedlingImage}
-            src="/images/seedling-icon.svg"
-          />
-        </div>
-      </div>
       <div>
-        <h2>React Garden Showcase</h2>
-        <div className={styles.grid}>
-          <Card />
-          <Card />
-          <Card />
-        </div>
+        <h1 className={styles.header}>
+          A garden where I grow React components.
+        </h1>
+        <p className={styles.tagline}>
+          Learning, experimenting, and creating better code - one component at a
+          time.
+        </p>
+      </div>
+
+      <div className={styles.grid}>
+        <Card />
+        <Card />
+        <Card />
+        <Card />
       </div>
     </section>
   );
